1c153a99089fe327ab65899adac42dbdc508922b
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-18 03:32:52 +0600

    class-validator class-transformer

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import {} from '@nestjs/common/decorators';
-
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signUp(@Body() dto: any) {
-    console.log(dto);
+  signUp(@Body() dto: AuthDto) {
     return this.authService.signup();
   }
 
